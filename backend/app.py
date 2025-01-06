3a0d87bf210b80af2f5f8d2b47632a278a76eb1f
Pritesh L Patil <prpa4901@colorado.edu>
2025-01-06 00:33:30 -0500

    first draft for today

---
+++ backend/app.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_community.llms import Ollama
+from langserve import add_routes
+
---
+++ chatbot/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+import os
+
+
+
