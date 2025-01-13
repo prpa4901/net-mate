07cf09f0509530c440eeb14f785961c481a01aa0
Pritesh L Patil <prpa4901@colorado.edu>
2025-01-13 02:18:04 -0500

    revised import differentiation

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.chat_models import ChatOllama
-from langchain_community.llms import Ollama
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langserve import add_routes
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+llm = ChatOllama(model="mistral", base_url="http://172.18.112.1:11434")
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system",
+     "You are an expertise in computer networking and internet with immense experience,"\
+      "excellent troubleshooter and design expert"),
+    ("human",
+     "Answer the question based on the following context in "\
+      "a most accurate way:\n{context}\n\nQuestion: {question}")
+])
+
 
--- chatbot/app.py
+++ chatbot/app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
+import requests
 import os
-
 
 
